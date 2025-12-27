3bf0ed566389bdd5b5e0382ba0cdab7daa25a09d
philct <philct00@gmail.com>
2025-12-27 07:50:48 -0500

    Make the form to submit data

--- plan/tasks/views.py
+++ plan/tasks/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render, redirect
 from django.http import request
 from django import forms
 
+tasks = []
+
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/task_list.html")
+    return render(request, "tasks/task_list.html", { 'tasks': tasks })
 
 def add_task(request):
-    form = NewTaskForm()
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            update_task = task
+            tasks.append(update_task)
+            return redirect("tasks:add_task")
+        else:
+            return render(request, "tasks/add_task.html", { 'form': form })
+
     return render(request, "tasks/add_task.html", 
                   {
-                    'form': form
+                    'form': NewTaskForm(),
                   },
                 )
