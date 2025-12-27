392d1da9e1d4814d45711d03e86934b1a97b47f3
philct <philct00@gmail.com>
2025-12-27 06:40:28 -0500

    Create add task page, create path & add_task function, create django form and connect it with the add task page

--- plan/tasks/views.py
+++ plan/tasks/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import request
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
     return render(request, "tasks/task_list.html")
+
+def add_task(request):
+    form = NewTaskForm()
+    return render(request, "tasks/add_task.html", 
+                  {
+                    'form': form
+                  },
+                )
