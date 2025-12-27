e16ead3823f6d9abebb3372d3425c1e60ba9db23
philct <philct00@gmail.com>
2025-12-27 08:35:00 -0500

    Finish session

--- plan/tasks/views.py
+++ plan/tasks/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import request
 from django import forms
 
-tasks = []
-
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/task_list.html", { 'tasks': tasks })
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+        
+    return render(request, "tasks/task_list.html", { 'tasks': request.session['tasks'] })
 
 def add_task(request):
     if request.method == 'POST':
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data['task']
-            update_task = task
-            tasks.append(update_task)
-            return redirect("tasks:add_task")
+            update_task = request.session['tasks']
+            update_task.append(task)
+            request.session['tasks'] = update_task
+            return redirect("tasks:index")
         else:
             return render(request, "tasks/add_task.html", { 'form': form })
 
