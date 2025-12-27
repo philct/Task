dcd2c7b00a045c843705a44c144eec35452ea71f
philct <philct00@gmail.com>
2025-12-27 06:06:07 -0500

    Create the path for the task_list page, connect with the index function of our app - located inside views.py, create a templates folder for our app, create the task_list.html and connect with the index function, also create a parent master.html to inherit the repeated code instead of writting them

--- plan/tasks/views.py
+++ plan/tasks/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import request
 
 # Create your views here.
+def index(request):
+    return render(request, "tasks/task_list.html")
